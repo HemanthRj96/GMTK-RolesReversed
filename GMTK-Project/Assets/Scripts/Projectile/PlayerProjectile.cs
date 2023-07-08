890a63448ab8ec2d3f45b4167526b8f75994b4c8
HemanthRj96 <hemanthrj96@gmail.com>
2023-07-08 14:41:41 +0530

    update 0.3
    projectile mechanics
    damage mechanics
    gameloop mechanics

---
+++ GMTK-Project/Assets/Scripts/Projectile/PlayerProjectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class PlayerProjectile : ProjectileBase
+{
+    protected override void OnHit(Collision collision)
+    {
+        var rb = GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.isKinematic = true;
+
+        if (collision.collider.TryGetComponent<Damageable>(out Damageable dmg))
+            dmg.ApplyDamage();
+
+        Destroy(gameObject);
+        GlobalEvents.Invoke_PlayerProjectileEnd();
+    }
+}
--- GMTK-Project/Assets/Scripts/Projectile/ProjectileBase.cs
+++ GMTK-Project/Assets/Scripts/Projectile/ProjectileBase.cs
@@ -1,8 +1,34 @@
 ﻿using System.Collections;
 using UnityEngine;
-
+using UnityEngine.Events;
 
 public class ProjectileBase : MonoBehaviour
 {
+    // fields
 
+    [SerializeField] private string _bounceSurfaceTag;
+
+    bool _hasHit = false;
+
+
+    // protected methods
+
+    protected virtual void OnHit(Collision collision) { }
+
+
+    // private methods
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.Equals(gameObject))
+            return;
+        if (collision.collider.CompareTag(_bounceSurfaceTag))
+            return;
+
+        if (!_hasHit)
+        {
+            _hasHit = true;
+            OnHit(collision);
+        }
+    }
 }
